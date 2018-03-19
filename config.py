a333497454c0b26077461f0ca9e4114d2d571adb
Daniel Briskin <daniel.briskin@gmail.com>
2018-03-18 21:58:04 -0400

    config.py and autoenv setup

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class ProductionConfig(Config):
+    DEBUG = False
