f07b3dfc9a4d71342ee4e4c7a1065618b259be6b
Lohit Talasila <ltalasila@uwaterloo.ca>
2018-03-22 11:00:02 -0400

    add attributes to location_db class

--- location_db.py
+++ location_db.py
@@ -9,38 +9,45 @@ from sqlalchemy.dialects.postgresql import JSON
 class Location_DB(DB.Model):
     __tablename__ = 'locations'
 
-    _id = DB.Column(DB.Integer, primary_key=True) # google id, api returns location with id 
+    _location_id = DB.Column(DB.Integer, primary_key=True) # google id, api returns location with id 
     _name = DB.Column(DB.String(32))
     _address = DB.Column(DB.String(32))
     _current_user_count = DB.Column(DB.Integer) 
+    _users_list = [] # list to track the number of users at the location
 
     # def __init__(self, name, surname, result_all, result_no_stop_words):
-    def __init__(self, email):
-        self.email = email
-        # self.result_all = result_all
-        # self.result_no_stop_words = result_no_stop_words
+    def __init__(self, location_id, name=None, address=None):
+        self._location_id = location_id 
+        self._name = name
+        self.address = address
+        self._current_user_count = 0
 
     def __repr__(self):
-        return '<id {}>'.format(self.id)
+        return '<location id {}>'.format(self._location_id)
 
-    def hash_password(self, password):
-        self.password_hash = pwd_context.encrypt(password)
+    def add_user_to_location(self, user_id):
+        self._current_user_count += 1 # increment user count at location 
+        self._users_list.append(user_id) # add user id to l of users at location
 
-    def verify_password(self, password):
-        return pwd_context.verify(password, self.password_hash)
 
-    def generate_auth_token(self, expiration = 600):
-        s = Serializer(current_app.config['SECRET_KEY'], expires_in = expiration)
-        return s.dumps({ 'id': self.id })
+    # def hash_password(self, password):
+    #     self.password_hash = pwd_context.encrypt(password)
 
-    @staticmethod
-    def verify_auth_token(token):
-        s = Serializer(current_app.config['SECRET_KEY'])
-        try:
-            data = s.loads(token)
-        except SignatureExpired:
-            return None    # valid token, but expired
-        except BadSignature:
-            return None    # invalid token
-        user = User.query.get(data['id'])
-        return user
+    # def verify_password(self, password):
+    #     return pwd_context.verify(password, self.password_hash)
+
+    # def generate_auth_token(self, expiration = 600):
+    #     s = Serializer(current_app.config['SECRET_KEY'], expires_in = expiration)
+    #     return s.dumps({ 'id': self.id })
+
+    # @staticmethod
+    # def verify_auth_token(token):
+    #     s = Serializer(current_app.config['SECRET_KEY'])
+    #     try:
+    #         data = s.loads(token)
+    #     except SignatureExpired:
+    #         return None    # valid token, but expired
+    #     except BadSignature:
+    #         return None    # invalid token
+    #     user = User.query.get(data['id'])
+    #     return user
