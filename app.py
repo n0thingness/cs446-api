a3b0d830e2117480082e2b34858812affa86863f
Daniel Briskin <daniel.briskin@gmail.com>
2018-03-27 16:15:52 -0400

    checkin

--- app.py
+++ app.py
@@ -189,8 +189,8 @@ def get_match():
 		matched_id = g.user.matchedUser
 		matched_user = User_DB.query.get(matched_id)
 		if matched_user is not None:
-			matched_name = g.user.matchedUser.name
-			matched_surname = g.user.matchedUser.surname
+			matched_name = matched_user.name
+			matched_surname = matched_user.surname
 	return jsonify(
 		id=matched_id,
 		name=matched_name,
